c2d710110a0c54f1ae015be9eabeec2a4cd47fc7
smskmrym <71767389+smskmrym@users.noreply.github.com>
2020-11-17 21:52:21 +0300

    2

--- proje/proje/Controllers/HomeController.cs
+++ proje/proje/Controllers/HomeController.cs
@@ -6,6 +6,7 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using proje.Models.ViewModels;
+using proje.Models;
 
 namespace proje.Controllers
 {
